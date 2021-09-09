9c0d860a1ffae4225dcc1bff29ae95f92f78ef13
whylost <whylost@aliyun.com>
2021-09-09 17:10:52 +0800

    📝实现 当调用effect()时返回 runner()函数

--- Front-end/mini-vue/src/reactivity/__tests__/effect.spec.ts
+++ Front-end/mini-vue/src/reactivity/__tests__/effect.spec.ts
@@ -9,7 +9,6 @@ describe('effect', () => {
         let nextAge
         effect(()=>{
             nextAge=user.age + 1
-            
         })
         expect(nextAge).toBe(11)
 
@@ -17,5 +16,25 @@ describe('effect', () => {
         user.age++
         expect(nextAge).toBe(12)
 
+    });
+
+    /**
+     * 调用 effect(fn) ->  返回一个 runner函数 
+     *  ->  手动调用 runner()时 ->  会调用effect(fn)内的 fn()
+     *  ->  返回fn()的返回值
+     */
+    it('should return runner when call effect', () => {
+        let foo = 0
+        const runner = effect(()=>{
+            foo++
+            return foo
+        })
+        // effect(fn)函数会默认执行一次fn()
+        expect(foo).toBe(1) 
+        // 调用effect(fn)返回的runner()函数会再次执行一次fn()
+        runner()
+        expect(foo).toBe(2)
+        // 调用effect(fn)返回的runner()函数会得到再次执行一次fn()后fn()的返回值
+        expect(runner()).toBe(3)
     });
 });
--- Front-end/mini-vue/src/reactivity/src/effect.ts
+++ Front-end/mini-vue/src/reactivity/src/effect.ts
@@ -1,26 +1,29 @@
-let activeEffect: ReactiveEffect
-class ReactiveEffect{
-    private _fn: Function;
-    constructor(fn:Function){
+
+let activeEffect: ReactiveEffect | undefined
+class ReactiveEffect<T = any>{
+    private _fn: ()=>T
+    constructor(fn: ()=>T){
         this._fn=fn
     }
     run(){
         activeEffect=this
-        this._fn()
+       return this._fn()
     }
 }
 /**
  * 副作用函数
  * @param fn 要执行的副作用 注意: 默认自执行一次 fn
  */
-export function effect(fn: Function){
+export function effect<T = any>(fn: ()=>T){
 
     const _effect = new ReactiveEffect(fn)
 
     _effect.run()
+    /* 返回当前 effect对应的的run()方法 */
+    return _effect.run.bind(_effect)
 }
 
-// TODO 这一段依赖收集的逻辑关系 需要多复习
+/* 这一段依赖收集的逻辑关系 需要多复习 */
 const targetMap = new Map()
 /**
  * 依赖收集
