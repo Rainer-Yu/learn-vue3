2150f60a57d6fde186ec7d78a28b52ce8e2607aa
whylost <whylost@aliyun.com>
2021-09-11 22:31:36 +0800

    🛠 格式化

--- Front-end/mini-vue/src/reactivity/src/effect.ts
+++ Front-end/mini-vue/src/reactivity/src/effect.ts
@@ -1,22 +1,21 @@
 type Dep = Set<ReactiveEffect | undefined>
 type KeyToDepMap = Map<any, Dep>
 let activeEffect: ReactiveEffect | undefined
-class ReactiveEffect<T = any>{
-    private _fn: ()=>T
-    constructor(fn: ()=>T){
-        this._fn=fn
+class ReactiveEffect<T = any> {
+    private _fn: () => T
+    constructor(fn: () => T) {
+        this._fn = fn
     }
-    run(){
-        activeEffect=this
-       return this._fn()
+    run() {
+        activeEffect = this
+        return this._fn()
     }
 }
 /**
  * 副作用函数
  * @param fn 要执行的副作用 注意: 默认自执行一次 fn
  */
-export function effect<T = any>(fn: ()=>T){
-
+export function effect<T = any>(fn: () => T) {
     const _effect = new ReactiveEffect(fn)
 
     _effect.run()
@@ -24,42 +23,42 @@ export function effect<T = any>(fn: ()=>T){
     return _effect.run.bind(_effect)
 }
 
-/* 这一段依赖收集的逻辑关系 需要多复习 */
-const targetMap = new WeakMap<object,KeyToDepMap>()
+// REVIEW  这一段依赖收集的逻辑关系 需要多复习
+const targetMap = new WeakMap<object, KeyToDepMap>()
 /**
  * 依赖收集
- * @param target 
- * @param key 
- */ 
-export function track(target:object,key:unknown){
+ * @param target
+ * @param key
+ */
+export function track(target: object, key: unknown) {
     /* 依赖对应关系: target -> key -> dep */
+    // anc
     let depsMap = targetMap.get(target)
-    if(!depsMap){
-        targetMap.set(target,(depsMap = new Map() as KeyToDepMap))
+    if (!depsMap) {
+        targetMap.set(target, (depsMap = new Map() as KeyToDepMap))
     }
     let dep = depsMap.get(key)
-    if(!dep){
-        depsMap.set(key,(dep = new Set() as Dep))
+    if (!dep) {
+        depsMap.set(key, (dep = new Set() as Dep))
     }
     dep.add(activeEffect)
-
 }
 
 /**
  * 触发依赖执行
  */
-export function  trigger(target:object,key:unknown){
+export function trigger(target: object, key: unknown) {
     let depsMap = targetMap.get(target)
 
-    if(!depsMap){
+    if (!depsMap) {
         return
     }
 
     let dep = depsMap.get(key)
-    if(dep&&dep?.size>0){
-        dep.forEach((effect)=>{
-            if(effect) effect?.run()
+    debugger
+    if (dep && dep.size > 0) {
+        dep.forEach((effect) => {
+            effect && effect?.run()
         })
     }
-    
 }
--- Front-end/mini-vue/src/reactivity/src/reactive.ts
+++ Front-end/mini-vue/src/reactivity/src/reactive.ts
@@ -1,21 +1,22 @@
-import {track, trigger} from "./effect"
+import { track, trigger } from './effect'
 
-export function reactive<T extends object>(raw:T):T{    /* 得到什么类型 返回什么类型 */
+export function reactive<T extends object>(raw: T): T {
+    /* 得到什么类型 返回什么类型 */
 
-    return new Proxy(raw,{
-        get(target, key){
+    return new Proxy(raw, {
+        get(target, key) {
             const res = Reflect.get(target, key)
 
             // 依赖收集
             track(target, key)
             return res
         },
-        set(target, key, value){
+        set(target, key, value) {
             const res = Reflect.set(target, key, value)
 
             // 触发依赖执行
             trigger(target, key)
             return res
-        }
+        },
     })
 }
